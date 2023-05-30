d09654ba7eddec3bdc1dfc1d1e3b03c7d1fe6433
anil karnam <anil.karnam03@gmail.com>
2023-05-30 17:13:00 -0400

    fingers x

--- today_append.py
+++ today_append.py
@@ -8,44 +8,34 @@ from datetime import datetime , timedelta, date
 # from dotenv import load_dotenv
 import os
 
-
-# load_dotenv()
-# secret_token = os.getenv('secret_token')
 secret_token = os.environ["token"]
 api_key = secret_token
 
 
 def main():
-    response = requests.get(f'https://api.metalpriceapi.com/v1/2022-05-21?api_key={api_key}&base=XAU')
-    print(response.status_code)
-    if response.status_code == 200:
-        print("Hello World")
-        print(response)
+	currentday = str(date.today())
+	d = {'XAU': 'Gold', 'XAG': 'Silver', 'XPT': 'Platinum', 'XPD': 'Palladium' }
+	for k in d:
+		url = f'https://api.metalpriceapi.com/v1/{currentday}?api_key={api_key}&base={k}'
+		response = requests.get(url).json()
+		today_time = response["timestamp"]
+		rates = response["rates"]
+		dt_obj =  currentday
+		new_data = {dt_obj : rates}
+		write_json(new_data,k)	
+   
+#function to add to JSON
+def write_json(new_data, k , filename='data.json'):
+	with open(filename,'r+') as file:
+		# First we load existing data into a dict.
+		file_data = json.load(file)
+		x = len(file_data[k])-1
+		# Join new_data with file_data inside emp_details
+		file_data[k][x]["rates"].update(new_data)
+		# Sets file's current position at offset.
+		file.seek(0)
+		# convert back to json.
+		json.dump(file_data, file, indent = 4)
 
 if __name__ == '__main__':
     main()
-    
-#function to add to JSON
-# def write_json(new_data, k , filename='data.json'):
-# 	with open(filename,'r+') as file:
-# 		# First we load existing data into a dict.
-# 		file_data = json.load(file)
-# 		x = len(file_data[k])-1
-# 		# Join new_data with file_data inside emp_details
-# 		file_data[k][x]["rates"].update(new_data)
-# 		# Sets file's current position at offset.
-# 		file.seek(0)
-# 		# convert back to json.
-# 		json.dump(file_data, file, indent = 4)
-
-
-# currentday = str(date.today())
-# d = {'XAU': 'Gold', 'XAG': 'Silver', 'XPT': 'Platinum', 'XPD': 'Palladium' }
-# for k in d:
-#     url = f'https://api.metalpriceapi.com/v1/{currentday}?api_key={api_key}&base={k}'
-#     response = requests.get(url).json()
-#     today_time = response["timestamp"]
-#     rates = response["rates"]
-#     dt_obj =  currentday
-#     new_data = {dt_obj : rates}
-#     write_json(new_data,k)	
